986f872c900d8b83ce742919a3b7b015a6cf701e
Joschka Schulz <Horstyschulz@gmail.com>
2015-05-23 22:32:16 +0200

    stupid merge after stash

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 var SoundcloudWrapper = require('./libs/soundcloud_wrapper');
 var PlayerWrapper = require('./libs/player_wrapper');
+var ProtonetListener = require('./libs/protonet_listener');
 var Server = require('./libs/server');
 
 
 var soundcloudWrapper = new SoundcloudWrapper();
 var playerWrapper = new PlayerWrapper();
 soundcloudWrapper.init();
+var protonet = new ProtonetListener();
+
+sc.init();
+protonet.init();
 
 function urlReceivedCallback (url) {
 	console.log('Received URL:' , url);
@@ -19,6 +24,15 @@ function urlReceivedCallback (url) {
 	});
 }
 
+protonet.listen(1000, function(url) {
+  sc.resolve(url, function(track_hash) {
+    //Getting stream_url and track_data
+    console.log("Added resolved URL: ", track_hash.stream_url)
+    playerWrapper.addPlaylist(track_hash);
+  }, function() {
+    console.log("Invalid URL");
+  });
+});
 
 var myServer = new Server(1337);
 myServer.init(urlReceivedCallback);
